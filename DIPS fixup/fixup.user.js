47261b3362050a18a7bba9c5c68738c23b2e4989
Brian McCauley <nobull67@gmail.com>
2014-10-14 19:34:44 +0100

    Clean up the cleanup. Put back the @grant. Removing it has no effect except to cause warnings that the @grant is missing. Put back the jQuery.noConflict(). I'm fairly sure that jQuery will reach out of the sandbox and change window.$ and window.jQuery. Remove useless calls to jQuery() where the argument is already a jQuery object. It feels clunky to use string manipulation set the height of #addednav when jQuery has a direct accessor. It feels clunky to add a #addcontact and then immediately search the DOM for it. Putting a &nbsp; into an empty <div> smells like cargo-cult unless I'm missing something. To we really want the navbar to have 1en of whitespace on the left?

--- DIPS fixup/fixup.user.js
+++ DIPS fixup/fixup.user.js
@@ -4,10 +4,16 @@
 // @description Fixup irritations in DIPS
 // @include     https://dips.sja.org.uk/*
 // @version     1
+// @grant       none
 // @require     https://code.jquery.com/jquery-2.1.1.min.js
 // ==/UserScript==
 
 // We don't require a separate context for jQuery, as without the @grant none, we will be in a sandbox.
+// ... exactly as we would be with the @grant none. The only difference is that with the explicit @grant none
+// we don't get warnings from the user script manager about there not being a @grant.
+
+// We still neet to tell jQuery that we want it only to play in our sandbox thus...
+this.$ = jQuery.noConflict(true);
 
 var pathArray = window.location.href.split('/');
 
@@ -17,23 +23,21 @@ baseURL += '/' + pathArray[3] + '/';
 
 var navHeight = 26;
 
-if (window.top == window.self) {
-    $(document).find('body')
-        .append(
-        '<div style="margin-top: 50px; position: absolute; bottom: 0; left: 0; height: '
-        + navHeight.toString()
-        + 'px; width:100%;" id="addednav">&nbsp</div>'
-    );
+if (window.top == window) {
+    $('<div style="margin-top: 50px; position: absolute; bottom: 0; left: 0; width:100%;" id="addednav">')
+    .height(navHeight)
+    .appendTo('body');
 } else {
-
-    var ournav = $(window.top.document).find('#addednav');
-    $(ournav).hide();
-
+    
+    var ournav = $('#addednav',window.top.document);
+    ournav.hide();
+    
     // Add Buttons here to the nav
-
+    
     if (window.location.href.match(/MessageMembers\.asp/)) {
-        $(ournav).append('<button id="addcontact">Add Contact Details</button>');
-        $(ournav).find('#addcontact').click(function() {
+        $('<button id="addcontact">Add Contact Details</button>')
+        .appendTo(ournav)
+        .click(function() {
             $('input[name^=member]').each(function() {
                 var row = $(this).closest('tr');
                 getMemberContact(this.value, function(data) {
@@ -43,18 +47,16 @@ if (window.top == window.self) {
             });
         });
     }
-
-    if ($(ournav).find("button")[0]) {
-        $(ournav).show();
-        $(ournav).closest('body').css('margin-bottom', (navHeight + 5).toString() + 'px');
-    }
-
+    
+    ournav.not(':empty').show().closest('body').css('margin-bottom', (navHeight + 5).toString() + 'px');
+    
     $(window).unload(function() {
-        $(ournav).find("button").remove();
-        $(ournav).hide();
-        $(ournav).closest('body').css('margin-bottom', '5px');
+        ournav.empty().hide().closest('body').css('margin-bottom', '5px');
     });
 }
+
+// Don't like a function starting 'get' (in lowercase) as that's conventionally reserved for (what pretend to be) property accessors in Java.
+// Yes I know Java is nothing to do with Javascript but nonetheless
 
 function getMemberContact(id, callback) {
     return $.get(baseURL + 'membermanagement-edit.asp?type=edit&number=' + id, function(page) {
@@ -62,7 +64,7 @@ function getMemberContact(id, callback) {
         $('input', page).each(function() {
             details[this.name] = this.value;
         });
-
+        
         callback(details);
     });
 }
