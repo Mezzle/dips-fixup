7efe2557278a51fc1044c2c86051f39e06371afc
Martin Meredith <martin@justsayplease.co.uk>
2016-05-11 17:42:02 +0100

    Further work on cleanup

--- DIPS fixup/fixup.user.js
+++ DIPS fixup/fixup.user.js
@@ -5,15 +5,11 @@
 // @include     https://dips.sja.org.uk/*
 // @version     1
 // @grant       none
-// @require     https://code.jquery.com/jquery-2.1.1.min.js
+// @require     https://code.jquery.com/jquery-2.2.3.min.js
 // ==/UserScript==
 
-// We don't require a separate context for jQuery, as without the @grant none, we will be in a sandbox.
-// ... exactly as we would be with the @grant none. The only difference is that with the explicit @grant none
-// we don't get warnings from the user script manager about there not being a @grant.
 
-// We still neet to tell jQuery that we want it only to play in our sandbox thus...
-this.$ = jQuery.noConflict(true);
+var $ = jQuery.noConflict(true);
 
 var pathArray = window.location.href.split('/');
 
@@ -21,50 +17,100 @@ var baseURL = window.location.origin ? window.location.origin : window.location.
 
 baseURL += '/' + pathArray[3] + '/';
 
+var DIPS = {
+    getMemberContact: function (id, callback) {
+        return $.get(
+            baseURL + 'membermanagement-edit.asp',
+            {
+                type: 'edit',
+                number: id
+            },
+            function (page) {
+                var details = {};
+                $('input', page).each(function () {
+                    details[this.name] = this.value;
+                });
+
+                callback(details);
+            }
+        );
+    },
+    getMemberDriving: function (id, callback) {
+        return $.get(
+            baseURL + 'MemberManagement-EditDrive.asp',
+            {
+                type: 'edit',
+                number: id
+            },
+            function (page) {
+                var summary = "";
+                var details = {};
+
+                $(':input', page).each(function () {
+                    var name = this.name;
+                    var val = $(this).val();
+
+                    details[name] = val;
+                    name = {"Car": "C", "Ambulance": "A", "Minibus": "M"}[name];
+                    
+                    val = {1: "F", 2: "O", 3: "R"}[val];
+                    
+                    if (name && val) {
+                        summary = summary + ' ' + name + '=' + val;
+                    }
+                });
+                
+                callback.call(details, summary.trim());
+            }
+        );
+    }
+};
+
 var navHeight = 26;
 
 if (window.top == window) {
-    $('<div style="margin-top: 50px; position: absolute; bottom: 0; left: 0; width:100%;" id="addednav">')
+    $('<div>')
+    .css({
+        marginTop: '50px',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%'
+    })
+    .attr('id', 'addednav')
     .height(navHeight)
     .appendTo('body');
 } else {
-    
-    var ournav = $('#addednav',window.top.document);
+    var ournav = $('#addednav', window.top.document);
+    var parentBody = ournav.closest('body');
+
     ournav.hide();
-    
-    // Add Buttons here to the nav
-    
+
     if (window.location.href.match(/MessageMembers\.asp/)) {
         $('<button id="addcontact">Add Contact Details</button>')
         .appendTo(ournav)
-        .click(function() {
-            $('input[name^=member]').each(function() {
+        .click(function () {
+            $('input[name^=member]').each(function () {
                 var row = $(this).closest('tr');
-                getMemberContact(this.value, function(data) {
-                    row.find('td:last').append($('<div>').append(data.PhoneNumber));
-                    row.find('td:eq(-2)').append($('<div>').append(data.eMailAddress));
+
+                row.find('.DFU-phone,.DFU-email').remove();
+
+                DIPS.getMemberContact(this.value, function (data) {
+                    row.find('td:last').append($('<div>').addClass('DFU-phone').append(data.PhoneNumber));
+                    row.find('td:eq(-2)').append($('<div>').addClass('DFU-email').append(data.eMailAddress));
                 });
             });
         });
     }
-    
-    ournav.not(':empty').show().closest('body').css('margin-bottom', (navHeight + 5).toString() + 'px');
-    
-    $(window).unload(function() {
-        ournav.empty().hide().closest('body').css('margin-bottom', '5px');
-    });
-}
 
-// Don't like a function starting 'get' (in lowercase) as that's conventionally reserved for (what pretend to be) property accessors in Java.
-// Yes I know Java is nothing to do with Javascript but nonetheless
+    if (ournav.not(':empty').length >= 1) {
+        ournav.not(':empty').show();
+        parentBody.css('margin-bottom', (navHeight + 5).toString() + 'px');
+    }
 
-function getMemberContact(id, callback) {
-    return $.get(baseURL + 'membermanagement-edit.asp?type=edit&number=' + id, function(page) {
-        var details = {};
-        $('input', page).each(function() {
-            details[this.name] = this.value;
-        });
-        
-        callback(details);
+    $(window).unload(function () {
+        ournav.empty();
+        ournav.hide();
+        ournav.closest('body').css('margin-bottom', '5px');
     });
 }
